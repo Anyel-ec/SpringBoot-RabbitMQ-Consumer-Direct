c7796af7fa62a6808bf7a6034e97c10c2b9866a0
anyel-ec <paulangel826@gmail.com>
2024-08-27 18:48:52 -0500

    add comment

--- src/main/java/top/anyel/consumer/config/RabbitMQConfigBinding.java
+++ src/main/java/top/anyel/consumer/config/RabbitMQConfigBinding.java
@@ -16,26 +16,29 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfigBinding {
 
+    // definimos las colas y los routing keys
     public static final String QUEUE_A = "queue.A";
     public static final String QUEUE_B = "queue.B";
     public static final String ROUTING_A = "routing.A";
     public static final String ROUTING_B = "routing.B";
 
+    // definimos los beans
     @Bean
-    Queue queueA(){
+    Queue queueA(){ // creamos la cola A
         return new Queue(QUEUE_A, false);
     }
 
     @Bean
-    Queue queueB(){
+    Queue queueB(){ // creamos la cola B
         return new Queue(QUEUE_B, false);
     }
 
     @Bean
-    DirectExchange exchange(){
+    DirectExchange exchange(){ // creamos el exchange
         return new DirectExchange("exchange.direct");
     }
 
+    // definimos los bindings para las colas y el exchange
     @Bean
     Binding binding(Queue queueA, DirectExchange exchange){
         return BindingBuilder.bind(queueA)
@@ -50,11 +53,13 @@ public class RabbitMQConfigBinding {
                 .with(ROUTING_B);
     }
 
+    // el Converter nos ayuda a convertir los mensajes a JSON
     @Bean
     MessageConverter messageConverter(){
         return new Jackson2JsonMessageConverter();
     }
 
+    // el RabbitTemplate nos ayuda a enviar mensajes
     @Bean
     RabbitTemplate rabbitTemplate(ConnectionFactory factory){
         RabbitTemplate rabbitTemplate = new RabbitTemplate(factory);
--- src/main/java/top/anyel/consumer/model/Message.java
+++ src/main/java/top/anyel/consumer/model/Message.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class Message {
+    // atributos
     private int id;
     private String name;
 }
