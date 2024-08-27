8185118c2e83b57073925d738c788bd1ac7d4e5f
anyel-ec <paulangel826@gmail.com>
2024-08-27 12:46:51 -0500

    start project in spring boot v3.3.3

---
+++ src/main/java/top/anyel/consumer/SpringBootRabbitMqConsumerDirectApplication.java
@@ -0,0 +1,13 @@
+package top.anyel.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRabbitMqConsumerDirectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRabbitMqConsumerDirectApplication.class, args);
+    }
+
+}
